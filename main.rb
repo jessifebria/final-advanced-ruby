e42ff41add3cd85085513de1c63c0c535b6eb861
Jessi <jessi@LAPTOP-CJG9BIOS.localdomain>
2021-02-09 13:45:56 +0700

    build_library|a|b|4 return Invalid Value, it must be an integer

--- main.rb
+++ main.rb
@@ -1,9 +1,16 @@
 require_relative 'library'
 
-def input_command(command)
-    command_arr = command.split("|")
-    if command_arr[0] == "build_library"
-        return command_arr.slice(1,3).map(&:to_i)
+def input_command(commands)
+    commands_arr = commands.split("|")
+    command = commands_arr[0]
+    value = commands_arr.slice(1,commands_arr.length)
+    if command == "build_library"
+        for i in value
+            if i.to_i.to_s != i 
+                return "Invalid Value, It must be an integer!"
+            end
+        end
+        return value.map(&:to_i)
     end
     return null
 end
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,9 +2,13 @@ require './main'
 
 describe "Main" do
     context "build_library" do
-        it "split build_library|2|1|3 command return 2,1,3" do
+        it "split build_library|2|1|3 command return [2,1,3]" do
             response = input_command("build_library|2|1|3")
             expect(response).to eq([2,1,3])
+        end
+        it "split build_library|a|b|4 command return Invalid Value, It must be an integer!" do
+            response = input_command("build_library|a|b|4")
+            expect(response).to eq("Invalid Value, It must be an integer!")
         end
     end 
 end
