1209408f0a6eccc81d9ff4461b345a39cfcbf534
Jessi <jessi@LAPTOP-CJG9BIOS.localdomain>
2021-02-27 19:49:52 +0700

    Create index to book location adapter

--- booklocation_to_index_adapter.rb
+++ booklocation_to_index_adapter.rb
@@ -1,13 +1,9 @@
 class BookLocationToIndexAdapter
-    
-    def initialize(book_location)
-        @book_location = book_location
-    end
 
-    def convert
-        shelter_index = @book_location[0..1].to_i -1
-        row_index = @book_location[2..3].to_i -1
-        column_index = @book_location[4..5].to_i -1
+    def convert(book_location)
+        shelter_index = book_location[0..1].to_i -1
+        row_index = book_location[2..3].to_i -1
+        column_index = book_location[4..5].to_i -1
         return shelter_index, row_index, column_index
     end
 end
