a46a1dac8bb1b772dfe8e4c90b04dafcf8846f65
Jessi <jessi@LAPTOP-CJG9BIOS.localdomain>
2021-02-27 23:00:10 +0700

    add take book in command pattern (librarycommand.rb)

--- command/librarycommand.rb
+++ command/librarycommand.rb
@@ -1,11 +1,13 @@
 require_relative 'build'
 require_relative 'putbook'
+require_relative 'takebook'
 
 class LibraryCommand
     def initialize
         @commands = Hash.new
         @commands["build_library"] = Build.new
         @commands["put_book"] = PutBook.new
+        @commands["take_book_from"] = TakeBook.new
     end
 
     def execute(command,params)
