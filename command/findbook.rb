b15b95eca803c1b7ae8458580ae30bd317f0d1c4
Jessi <jessi@LAPTOP-CJG9BIOS.localdomain>
2021-02-28 10:29:10 +0700

    create test case for findbook

--- command/findbook.rb
+++ command/findbook.rb
@@ -1,15 +1,57 @@
 require_relative 'command'
 require './library'
 require './book'
-require './booklocation_to_index_adapter'
+require './index_to_booklocation_adapter'
 
 class FindBook < Command
 
     def validate(params)
-        
+        if (params.length != 1) or (num?(params[0].to_s) == false) or params[0].to_i<1
+            return "Invalid Value!"
+        end
+    
+        @isbn = params[0]
+
+        return "valid"
     end
 
     def execute(params)
-        
+        message = validate(params)
+        if message != "valid"
+            return message
+        end
+
+        library = Library.instance
+        shelters = library.shelters
+
+        shelter_index = 0
+        row_index = 0
+        flag = 0
+        for shelter in shelters do
+            for rows in shelter do
+                row_index = 0
+                for book in rows do
+                    if book != 0
+                        if book.isbn == @isbn
+                            flag = 1
+                            break
+                        end
+                    end
+                end
+                row_index+=1
+            end
+            if flag == 1
+                break
+            end
+            shelter_index+=1
+        end
+        if flag == 0 
+            return "Book not found!"
+        end
+
+        adapter = IndexToBookLocationAdapter.new
+
+        return "Found the book at #{adapter.convert(shelter_index, row_index, column_index)}"
+
     end
 end
---
+++ spec/findbook_spec.rb
@@ -0,0 +1,61 @@
+require './command/findbook'
+require './main'
+
+describe "Find Book" do
+
+    before do
+        @findbook = FindBook.new
+        @main = Main.new
+        Library.reset
+    end
+    
+    let (:build_library) {@main.input_command("build_library|2|1|3")}
+    let (:put_book) {@main.input_command("put_book|9780747532743|Harry Potter 1|J. K. Rowling")}
+    let (:put_book2){ @main.input_command("put_book|9780807281918|Harry Potter 2|J. K. Rowling") }
+    let (:put_book3){ @main.input_command("put_book|9780739330944|Eragon 1|Christopher Paolini")}
+    let (:put_book4){ @main.input_command("put_book|9780545582933|Harry Potter 3|J. K. Rowling") }
+    let (:put_book5){ @main.input_command("put_book|9780132350884|Clean Code|Robert Cecil Martin")}
+    let (:put_book6){ @main.input_command("put_book|9780201485677|Refactoring|Martin Fowler, Kent Beck")}
+
+    context "validate params" do
+        it "give params>1 return Invalid Value" do
+            response = @findbook.validate(["34","23"])
+            expect(response).to eq("Invalid Value!")
+        end
+        it "give not an integer param return Invalid Value!" do
+            response = @findbook.validate(["aa"])
+            expect(response).to eq("Invalid Value!")
+        end
+        it "give int param, only 1, but negative return Invalid Value!" do
+            response = @findbook.validate(["-11"])
+            expect(response).to eq("Invalid Value!")
+        end
+        it "valid param return valid" do
+            response = @findbook.validate(["91239872311"])
+            expect(response).to eq("valid")
+        end
+    end 
+
+    context "execute" do
+        it "find_book|9780807281918 return Found the book at 010102" do
+            build_library
+            put_book
+            put_book2
+            put_book3
+            put_book4
+            put_book5
+            response = @findbook.execute(["9780807281918"])
+            expect(response).to eq("Found the book at 010102")
+        end
+        it "find_book|100 return Book not found!" do
+            build_library
+            put_book
+            put_book2
+            put_book3
+            put_book4
+            response = @findbook.execute(["100"])
+            expect(response).to eq("Book not found!")
+        end
+    end 
+
+end
