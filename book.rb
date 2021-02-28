fd7725cfc7443a84b1d77a7e5d20e9c7518829da
Jessi <jessi@LAPTOP-CJG9BIOS.localdomain>
2021-02-28 18:45:58 +0700

    create method is_author_same? and is_title_same? in book.rb

--- book.rb
+++ book.rb
@@ -12,6 +12,20 @@ class Book
         end
         false
     end
+    
+    def is_author_same?(author_from_user)
+        if author == author_from_user
+            return true
+        end
+        false
+    end
+    
+    def is_title_same?(title_from_user)
+        if title == title_from_user
+            return true
+        end
+        false
+    end
 
     
 end
