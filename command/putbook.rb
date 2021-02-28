e94a5e4090679dbd95227339795e69c815a85a86
Jessi <jessi@LAPTOP-CJG9BIOS.localdomain>
2021-02-28 08:58:02 +0700

    remove inappropiate intimacy in putbook.rb

--- command/putbook.rb
+++ command/putbook.rb
@@ -29,26 +29,7 @@ class PutBook < Command
         end
         book = Book.new(@isbn,@title,@author)
         library = Library.instance
-        shelters = library.shelters
-
-        shelter_index = 0
-        row_index = 0
-        for shelter in shelters do
-            for rows in shelter do
-                row_index = 0
-                column_index = rows.index("0")
-                if column_index.is_a?Integer 
-                    flag = 1
-                    shelters[shelter_index][row_index][column_index] = book
-                    break
-                end
-                row_index+=1
-            end
-            if flag == 1
-                break
-            end
-            shelter_index+=1
-        end
+        shelter_index, row_index, column_index = library.put_book(book)
         
         return "Allocated address: #{(shelter_index+1).to_s.rjust(2, "0")}#{(row_index+1).to_s.rjust(2, "0")}#{(column_index+1).to_s.rjust(2, "0")}"
     end
--- library.rb
+++ library.rb
@@ -1,4 +1,5 @@
 require_relative 'index_to_booklocation_adapter'
+require_relative 'book'
 
 class Library
     attr_accessor :shelters
@@ -29,6 +30,28 @@ class Library
         @@instance 
     end
 
+    def put_book(book)
+        shelter_index = 0
+        row_index = 0
+        for shelter in @shelters do
+            for rows in shelter do
+                row_index = 0
+                column_index = rows.index("0")
+                if column_index.is_a?Integer 
+                    flag = 1
+                    @shelters[shelter_index][row_index][column_index] = book
+                    break
+                end
+                row_index+=1
+            end
+            if flag == 1
+                break
+            end
+            shelter_index+=1
+        end
+        return shelter_index, row_index, column_index
+    end
+
     def take_book(shelter_index, row_index, column_index)
         book_location = @ToBookLocationAdapter.convert(shelter_index, row_index, column_index)
         if @shelters[shelter_index][row_index][column_index] == "0"
@@ -45,6 +68,8 @@ class Library
         end
         return false
     end
+
+
 
     private def createshelter
         Array.new(@n) {Array.new(@x) {Array.new(@y){"0"}}}
