4d182bef2adf3121021812039a53c9ed7eee77a2
Jessi <jessi@LAPTOP-CJG9BIOS.localdomain>
2021-02-28 12:04:01 +0700

    edit execute methods in librarycommand to receive defauly params

--- command/librarycommand.rb
+++ command/librarycommand.rb
@@ -2,6 +2,7 @@ require_relative 'build'
 require_relative 'putbook'
 require_relative 'takebook'
 require_relative 'findbook'
+require_relative 'listbooks'
 
 class LibraryCommand
     def initialize
@@ -10,9 +11,15 @@ class LibraryCommand
         @commands["put_book"] = PutBook.new
         @commands["take_book_from"] = TakeBook.new
         @commands["find_book"] = FindBook.new
+        @commands["list_books"] = ListBooks.new
     end
 
-    def execute(command,params)
-        @commands[command].execute(params)
+    def execute(command,params=nil)
+        if params==nil
+            @commands[command].execute
+        else
+            @commands[command].execute(params)
+        end
     end
+
 end
