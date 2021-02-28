2b9524f46b8770b53d88ae7ed9f0e3042aa17d4a
Jessi <jessi@LAPTOP-CJG9BIOS.localdomain>
2021-02-28 10:14:36 +0700

    create findbook.rb

---
+++ command/findbook.rb
@@ -0,0 +1,15 @@
+require_relative 'command'
+require './library'
+require './book'
+require './booklocation_to_index_adapter'
+
+class FindBook < Command
+
+    def validate(params)
+        
+    end
+
+    def execute(params)
+        
+    end
+end
