d8d4593bee439e62f9165cbcce09b9662e9d0d6b
Jessi <jessi@LAPTOP-CJG9BIOS.localdomain>
2021-02-28 18:03:27 +0700

    refactoring inappropiate intimacy in listbooks.rb

--- command/listbooks.rb
+++ command/listbooks.rb
@@ -7,22 +7,8 @@ require './printer/printerfactory'
 class ListBooks
 
     def execute
-        adapter = IndexToBookLocationAdapter.new
-        response = ""
         library = Library.instance
-        shelters = library.shelters
-        hash_of_books = Hash.new
-        for shelter_index in 0...shelters.length do
-            for row_index in 0...shelters[shelter_index].length do
-                for column_index in 0...shelters[shelter_index][row_index].length do
-                    if shelters[shelter_index][row_index][column_index] != 0
-                        book_location = adapter.convert(shelter_index,column_index, row_index)
-                        hash_of_books[book_location] = shelters[shelter_index][row_index][column_index]
-                    end
-                end
-            end
-        end
-
+        hash_of_books = library.get_hash_of_books
         printer = PrinterFactory.new_printer('library')
         response = printer.print(hash_of_books)
 
--- library.rb
+++ library.rb
@@ -67,7 +67,22 @@ class Library
         return -1,-1,-1
     end
 
-
+    def get_hash_of_books
+        response = ""
+        hash_of_books = Hash.new
+        for shelter_index in 0...@shelters.length do
+            for row_index in 0...@shelters[shelter_index].length do
+                for column_index in 0...@shelters[shelter_index][row_index].length do
+                    if @shelters[shelter_index][row_index][column_index] != 0
+                        book_location = @ToBookLocationAdapter.convert(shelter_index,column_index, row_index)
+                        hash_of_books[book_location] = @shelters[shelter_index][row_index][column_index]
+                    end
+                end
+            end
+        end
+        return hash_of_books
+    end
+    
     def is_index_exceed?(shelter_index, row_index, column_index)
         if (shelter_index >= @n) or (row_index >= @x) or (column_index >= @y)
             return true
