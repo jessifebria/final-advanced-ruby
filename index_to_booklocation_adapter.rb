6ed82ae5c9246a3a7e570f179b48b3a13ab7c54e
Jessi Febria <jfebria2302@gmail.com>
2021-03-03 13:27:01 +0700

    Delete index_to_booklocation_adapter.rb

--- index_to_booklocation_adapter.rb
+++
@@ -1,7 +0,0 @@
-class IndexToBookLocationAdapter
-
-    def convert(shelter_index, row_index, column_index)
-        book_location = "#{(shelter_index+1).to_s.rjust(2, "0")}#{(row_index+1).to_s.rjust(2, "0")}#{(column_index+1).to_s.rjust(2, "0")}"
-        return book_location
-    end
-end
