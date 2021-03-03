ff65492b5c89a0fa7e1b4c3a3425a522ee802474
Jessi Febria <jfebria2302@gmail.com>
2021-03-03 13:39:49 +0700

    Delete spec directory

--- spec/printer_spec.rb
+++
@@ -1,8 +0,0 @@
-require '../advanced_final/app/printer_factory/printer'
-
-describe 'Printer' do
-  it 'return not implemented yet' do
-    printer = Printer.new
-    expect(printer.print('a')).to eq('Not Implemented Yet')
-  end
-end
